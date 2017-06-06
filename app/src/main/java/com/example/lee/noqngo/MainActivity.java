ea62b206ec4922e6e5d644a939a8a24e5543aec8
LEEKYOUNGHWA <rudghk5220@gmail.com>
2017-06-06 16:40:10 +0900

    디자인수정

--- app/src/main/java/com/example/lee/noqngo/MainActivity.java
+++ app/src/main/java/com/example/lee/noqngo/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.lee.noqngo;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -101,14 +99,6 @@ public class MainActivity extends AppCompatActivity
             }
         };
 
-        //고객호출
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "고객호출", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
 
         //드로어
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -248,9 +238,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
